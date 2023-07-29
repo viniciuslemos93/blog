052d405815fad5fb37a52814cec2cfc7bd00fba5
viniciuslemos93 <viniciuslemos.dev@gmail.com>
2023-07-29 10:25:17 -0300

    commit inicial

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+    include_once("helpers/url.php");
+?>
+
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="<?= $BASE_URL ?>/style/styles.css">
+    <title>Projeto Blog</title>
+</head>
+<body>
+    
+</body>
+</html>
