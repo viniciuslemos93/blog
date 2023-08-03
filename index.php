2522912fa3c3d52e257a9d17c0a67152b61c9ef4
viniciuslemos93 <viniciuslemos.dev@gmail.com>
2023-08-02 21:11:27 -0300

    style: Estilzado o Header e o Footer

--- index.php
+++ index.php
@@ -2,10 +2,8 @@
 include_once("templates/header.php");
 ?>
 
-<body>
     <H1>Olá mundo!</H1>
-    <?php
-    include_once("templates/footer.php");
-    ?>
-</body>
-</html>
+
+<?php
+include_once("templates/footer.php")
+?>
